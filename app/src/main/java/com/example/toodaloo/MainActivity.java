948dbe07203718522012dc9d0f2ca0c5fb04d0e5
PaulLeung93 <78509075+PaulLeung93@users.noreply.github.com>
2021-11-13 15:37:09 -0500

    Map Activity set up. Includes implementing Google map/location dependecies

--- app/src/main/java/com/example/toodaloo/MainActivity.java
+++ app/src/main/java/com/example/toodaloo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.toodaloo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openMap(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
 }
