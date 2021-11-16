e68447e09d503f30feb955e0d8ca435347bb6b0f
Jessie Su <Jessiesu3@gmail.com>
2021-11-15 22:53:42 -0500

    Added Navigation Bar, there was an issue with my branch

--- app/src/main/java/com/example/toodaloo/MainActivity.java
+++ app/src/main/java/com/example/toodaloo/MainActivity.java
@@ -1,17 +1,52 @@
 package com.example.toodaloo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch(menuItem.getItemId()){
+                    case R.id.action_map:
+                        Toast.makeText(MainActivity.this, "MAP!", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case R.id.action_profile:
+                        Toast.makeText(MainActivity.this, "PROFILE!", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case R.id.action_feed:
+                        Toast.makeText(MainActivity.this,"FEED", Toast.LENGTH_SHORT).show();
+
+                        break;
+                }
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_map);
+
+
     }
 
     public void openMap(View view){
