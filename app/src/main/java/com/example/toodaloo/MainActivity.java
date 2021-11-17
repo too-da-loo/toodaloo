a2dcc8c8bc3989a438bdfe711e9d42543ff75313
Jessie Su <Jessiesu3@gmail.com>
2021-11-17 00:06:39 -0500

    Added profile and feed

--- app/src/main/java/com/example/toodaloo/MainActivity.java
+++ app/src/main/java/com/example/toodaloo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.toodaloo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,37 +11,42 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.toodaloo.fragments.FeedFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
+
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Fragment fragment;
+                Fragment fragment = null;
                 switch(menuItem.getItemId()){
                     case R.id.action_map:
                         Toast.makeText(MainActivity.this, "MAP!", Toast.LENGTH_SHORT).show();
-
+                        fragment = new Fragment();
                         break;
                     case R.id.action_profile:
                         Toast.makeText(MainActivity.this, "PROFILE!", Toast.LENGTH_SHORT).show();
-
+                        fragment = new FeedFragment();
                         break;
                     case R.id.action_feed:
                         Toast.makeText(MainActivity.this,"FEED", Toast.LENGTH_SHORT).show();
-
+                        fragment = new FeedFragment();
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
