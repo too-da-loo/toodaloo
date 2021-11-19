e9df76ca7d40fab884db3dccac7cca99e2f332a9
AliceLiu17 <alice.liu37@gmail.com>
2021-11-18 20:27:35 -0500

    Login Activity without Parse

--- app/src/main/java/com/example/toodaloo/LoginActivity.java
+++ app/src/main/java/com/example/toodaloo/LoginActivity.java
@@ -1,4 +1,55 @@
 package com.example.toodaloo;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.security.PublicKey;
+
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnSignUp;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"onClick Login Button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser (username, password);
+            }
+        });
+    }
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user" + username);
+
+    }
+
+    private void goMainActivity(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
 }
